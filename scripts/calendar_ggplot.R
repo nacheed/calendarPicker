7cc1a3dc061e50da1fbe79c44f2e2e2f09f5c792
nacheed <nabdousalami@yahoo.com>
2017-11-03 16:31:37 +0100

    add value column

--- scripts/calendar_ggplot.R
+++ scripts/calendar_ggplot.R
@@ -18,8 +18,8 @@ library( ggplot2 )
 
 # Creation data -----------------------------------------------------------
 
-calendar <- expand.grid(JOURS = c("Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim"), SEMAINES = seq_len(52))
+calendar <- expand.grid(JOURS = c("Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim"), SEMAINES = seq_len(52), KEEP.OUT.ATTRS = FALSE)
 head(calendar)
 
-
-
+calendar$VALEUR <- 0
+str(calendar)
